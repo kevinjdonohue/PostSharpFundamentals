e227188109f3247bd2af5ff967b88ed4fcbc98ee
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-01-10 20:35:43 -0800

    Updated/Reorganized solution to contain 2 projects - one for example code and one for unit tests

--- PostSharpExamplesTests/Class1.cs
+++ PostSharpExamplesTests/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PostSharpExamples
+namespace PostSharpExamplesTests
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
--- PostSharpExamplesTests/Properties/AssemblyInfo.cs
+++ PostSharpExamplesTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PostSharpExamples")]
+[assembly: AssemblyTitle("PostSharpExamplesTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PostSharpExamples")]
+[assembly: AssemblyProduct("PostSharpExamplesTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bd08b91-d932-4e22-b1db-ef45008ce626")]
+[assembly: Guid("6f89f76c-f81b-4cef-89a8-6ecd01127f3e")]
 
 // Version information for an assembly consists of the following four values:
 //
